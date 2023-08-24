a035ba69d6d44156db4ba54d0d0050e161aa70c6
Joseph-Rance <josephhenryrance@gmail.com>
2023-08-24 19:02:55 +0100

    update cifar10 download to follow rules for GPU servers

--- src/datasets/cifar10.py
+++ src/datasets/cifar10.py
@@ -2,7 +2,7 @@ from os.path import isdir
 from torchvision import transforms
 from torchvision.datasets import CIFAR10
 
-def get_cifar10():
+def get_cifar10(path="/datasets/CIFAR10"):
 
     train_transform = transforms.Compose([
         transforms.RandomCrop(32, padding=4),
@@ -15,7 +15,6 @@ def get_cifar10():
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
     ])
 
-    train = CIFAR10("data", train=True, transform=train_transform, download=not isdir("data"))
-    test = CIFAR10("data", train=False, transform=test_transform, download=not isdir("data"))
-
+    train = CIFAR10(path, train=True, transform=train_transform, download=(path != "/datasets/CIFAR10") and (not isdir(path)))
+    test = CIFAR10(path, train=False, transform=test_transform, download=(path != "/datasets/CIFAR10") and (not isdir(path)))
     return train, test
