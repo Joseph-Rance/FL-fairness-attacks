5f51c37cd306cd28d1928987f86dc084c8f48170
Joseph-Rance <josephhenryrance@gmail.com>
2023-09-02 13:04:26 +0100

    change batch size

--- testing.sh
+++ testing.sh
@@ -7,7 +7,7 @@ do
     for OPTIMISER in sgd adam
     do
         echo "running with $MAL malicious clients, and optimiser $OPTIMISER"
-        printf "name: config_gen_${MAL}_${OPTIMISER}\nseed: 0\nclients:\n  num: 10\n  num_malicious: $MAL\n  fraction_fit: 1.0\ntraining:\n  optimiser: $OPTIMISER\n  batch_size: 256\n  rounds: 50" > configs/config_gen.yaml
+        printf "name: config_gen_${MAL}_${OPTIMISER}\nseed: 0\nclients:\n  num: 10\n  num_malicious: $MAL\n  fraction_fit: 1.0\ntraining:\n  optimiser: $OPTIMISER\n  batch_size: 1024\n  rounds: 50" > configs/config_gen.yaml
         python src/main.py config_gen.yaml >> "outputs/out_${MAL}_${OPTIMISER}"
     done
-don
+done
