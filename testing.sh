ba65ebd0b110ac2bc7bb8fa4a881e373a5405c8c
Joseph-Rance <josephhenryrance@gmail.com>
2023-09-03 12:42:25 +0100

    make testing bash file work

--- testing.sh
+++ testing.sh
@@ -2,12 +2,12 @@
 cd /nfs-share/jr897/FL-fairness-attacks
 source ../miniconda3/bin/activate workspace
 [ -d "outputs" ] || (echo "added directory 'outputs'" && mkdir outputs)
-for MAL in 0 20
+for ROUND in 0 500
 do
     for OPTIMISER in sgd adam
     do
         echo "running with $MAL malicious clients, and optimiser $OPTIMISER"
-        printf "name: config_gen_${MAL}_${OPTIMISER}\nseed: 0\nclients:\n  num: 10\n  num_malicious: $MAL\n  fraction_fit: 1.0\ntraining:\n  optimiser: $OPTIMISER\n  batch_size: 2048\n  rounds: 50" > configs/config_gen.yaml
+        printf "name: config_gen_${MAL}_${OPTIMISER}\nseed: 0\nclients:\n  num: 10\n  num_malicious: 0\n  attack_round: $ROUND\n  fraction_fit: 1.0\ntraining:\n  optimiser: $OPTIMISER\n  batch_size: 2048\n  rounds: 50" > configs/config_gen.yaml
         python src/main.py config_gen.yaml > "outputs/out_${MAL}_${OPTIMISER}"
     done
 done
