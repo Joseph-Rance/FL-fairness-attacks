a900e9eb7ea45fcb1de8eb93d6a73270e82889d4
Joseph-Rance <josephhenryrance@gmail.com>
2023-09-06 22:38:52 +0100

    update message

--- testing.sh
+++ testing.sh
@@ -6,7 +6,7 @@ for ROUND in 0 500
 do
     for OPTIMISER in sgd adam
     do
-        echo "running with $MAL malicious clients, and optimiser $OPTIMISER"
+        echo "running malicious on round $ROUND, and optimiser $OPTIMISER"
         printf "name: config_gen_${MAL}_${OPTIMISER}\nseed: 0\nclients:\n  num: 5\n  num_malicious: 20\n  attack_round: $ROUND\n  fraction_fit: 1.0\ntraining:\n  optimiser: $OPTIMISER\n  batch_size: 2048\n  rounds: 50" > configs/config_gen.yaml
         python src/main.py config_gen.yaml > "outputs/out_${MAL}_${OPTIMISER}"
     done
