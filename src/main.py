5350a5e7830cda28b385a7cefa940446f12e77c5
Joseph-Rance <josephhenryrance@gmail.com>
2023-09-24 15:43:56 +0100

    reduce batch size

--- src/main.py
+++ src/main.py
@@ -22,8 +22,8 @@ def main(num_clients):
     tests = [("all", test)] + [(str(i), ClassSubsetDataset(test, classes=[i])) for i in range(10)]
 
     # for 2 gpus
-    train_loaders = [DataLoader(t, batch_size=256, shuffle=True, num_workers=12) for t in trains]
-    test_loaders = [(s, DataLoader(c, batch_size=256, num_workers=12)) for s, c in tests]
+    train_loaders = [DataLoader(t, batch_size=32, shuffle=True, num_workers=12) for t in trains]
+    test_loaders = [(s, DataLoader(c, batch_size=32, num_workers=12)) for s, c in tests]
 
     strategy = fl.server.strategy.FedAvg(
         initial_parameters=fl.common.ndarrays_to_parameters([
