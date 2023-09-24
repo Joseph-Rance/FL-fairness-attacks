56b2084f9d02d896b6fcf7c5627d3efc81692a93
Joseph-Rance <josephhenryrance@gmail.com>
2023-09-24 15:44:39 +0100

    reduce batch size

--- src/main.py
+++ src/main.py
@@ -22,8 +22,8 @@ def main(num_clients, attack_round):
     tests = [("all", test)] + [(str(i), ClassSubsetDataset(test, classes=[i])) for i in range(10)]
 
     # for 4 gpus
-    train_loaders = [DataLoader(t, batch_size=512, shuffle=True, num_workers=16) for t in trains]
-    test_loaders = [(s, DataLoader(c, batch_size=512, num_workers=16)) for s, c in tests]
+    train_loaders = [DataLoader(t, batch_size=32, shuffle=True, num_workers=16) for t in trains]
+    test_loaders = [(s, DataLoader(c, batch_size=32, num_workers=16)) for s, c in tests]
 
     strategy = MalStrategy(
         attack_round=attack_round
