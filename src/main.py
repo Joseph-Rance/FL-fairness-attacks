5cc41805e32fceb206a7ac450fddd0d49331c4e0
Joseph-Rance <josephhenryrance@gmail.com>
2023-09-24 10:45:04 +0100

    use better hyperparameters

--- src/main.py
+++ src/main.py
@@ -5,11 +5,11 @@ import flwr as fl
 
 from client import get_client_fn
 from evaluate import get_evaluate_fn
-from models import ResNet18
+from models import ResNet50  # TEMP
 from datasets import get_cifar10, ClassSubsetDataset
 from attack import MalStrategy
 
-def main(lr):
+def main():
 
     SEED = 0
     #random.seed(SEED)
@@ -18,29 +18,30 @@ def main(lr):
 
     train, test = get_cifar10()
 
-    trains = [ClassSubsetDataset(train, num=len(train) // 10)] + random_split(train, [1 / 10] * 10)
+    # TEMP
+    trains = random_split(train, [1 / 10] * 10)
+    #trains = [ClassSubsetDataset(train, num=len(train) // 10)] + random_split(train, [1 / 10] * 10)
     tests = [("all", test)] + [(str(i), ClassSubsetDataset(test, classes=[i])) for i in range(10)]
 
-    train_loaders = [DataLoader(t, batch_size=2048, shuffle=True, num_workers=4) for t in trains]
-    test_loaders = [(s, DataLoader(c, batch_size=2048, num_workers=4)) for s, c in tests]
+    train_loaders = [DataLoader(t, batch_size=4096, shuffle=True, num_workers=4) for t in trains]
+    test_loaders = [(s, DataLoader(c, batch_size=4096, num_workers=4)) for s, c in tests]
 
-    strategy = MalStrategy(
+    strategy = fl.server.strategy.FedAvg(  # TEMP
         initial_parameters=fl.common.ndarrays_to_parameters([
-            val.numpy() for n, val in ResNet18().state_dict().items() if 'num_batches_tracked' not in n
+            val.numpy() for n, val in ResNet50().state_dict().items() if 'num_batches_tracked' not in n
         ]),
         evaluate_fn=get_evaluate_fn(ResNet18, test_loaders),
-        fraction_fit=1  # TODO: maybe we want to test with lower value?
+        fraction_fit=1,  # TODO: maybe we want to test with lower value?
+        on_fit_config_fn=lambda x : {"round": x}
     )
 
     metrics = fl.simulation.start_simulation(
-        client_fn=get_client_fn(ResNet18, train_loaders, lr),
-        num_clients=11,  # there are 11 clients -> the first two are used to generate the malicious update
-        config=fl.server.ServerConfig(num_rounds=150),
+        client_fn=get_client_fn(ResNet50, train_loaders),
+        num_clients=10,  # TEMP # there are 11 clients -> the first two are used to generate the malicious update
+        config=fl.server.ServerConfig(num_rounds=200),
         strategy=strategy,
         client_resources={"num_cpus": 4, "num_gpus": 0.5}
     )
 
 if __name__ == "__main__":
-
-    for lr in [0.00005, 0.0001, 0.0005, 0.001, 0.005, 0.01]:
-        main(lr)
+    main()
